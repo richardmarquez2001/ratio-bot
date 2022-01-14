94710f84154286db5f2299df854ce5a2a8ce3184
richardmarquez2001 <richard.marquez.2001@gmail.com>
2022-01-14 03:00:09 -0500

    Add two new conds

--- index.js
+++ index.js
@@ -11,18 +11,70 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
+	if (message.author.bot) return;
+	// message that will be returned eventually
+	const messageSend = [];
+
+	// used for help message (message reply)
+	let helpCond = true;
 	try {
 		const parsedMessage = message.content.toLowerCase();
 
-		// second cond prevents bot from ratioing itself
-		if (parsedMessage.includes('ratio') && message.author.id !== '930038766184304650') {
-			message.channel.send('ratio');
+		// second cond prevents bot from ratioing itself, third cond prevents bot from ratioing me (richard)
+		if (parsedMessage.includes('ratio') && message.author.id !== '182594613838610432') {
+			messageSend.push('ratio');
 		}
-		else if (message.mentions.has('182594613838610432')) {
-			message.channel.send('noti');
+		// when someone pings ric
+		if (message.mentions.has('182594613838610432')) {
+			messageSend.push('noti');
+		}
+		// when someone types in braindead
+		if (parsedMessage.includes('braindead')) {
+			messageSend.push('i agree');
+		}
+		// when someone replies
+		if (message.type == 'REPLY') {
+			// generic help message
+			if (message.content == 'help me') {
+				helpCond = false;
+				message.channel.send('```Hello! I am the ratio bot, created by Ric. Here are some things you can type, and test out!: \n- "ratio"\n- "noti"\n- "braindead"\n- replying to my message!\n\nIf you have more things you\'d like to add, please message ric.```');
+			}
+			// for 'can i get a ___ ' condition
+			else {
+				const msgArr = parsedMessage.split(' ');
+				let defaultCond = true;
+				try {
+
+					if (msgArr.length > 4) {
+						// prefix checks for "can i get a"
+						const prefix = msgArr.splice(0, 4);
+						if (prefix.join(' ').toLocaleLowerCase() == 'can i get a') {
+							// gets the second half of the message
+							messageSend.push(msgArr.join(' '));
+
+							defaultCond = false;
+							// prevents default message from being sent
+						}
+					}
+
+				}
+				// silent return error
+				catch (err) {
+					message.channel('Somebody tell <@!182594613838610432> a glitch happened');
+					return;
+				}
+				// default message
+				if (defaultCond == true) messageSend.push('chill chill');
+			}
+		}
+
+		if (messageSend.length != 0 && helpCond) {
+
+			message.channel.send(messageSend.join(' + '));
 		}
 	}
 	catch (err) {
+		message.channel('Somebody tell <@!182594613838610432> a glitch happened');
 		console.log(err);
 	}
 
